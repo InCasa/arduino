43c6289a530ece199490dc5aeb2937afd1cacef4
Abumeragan <rodrigo.uba@gmail.com>
2016-08-31 14:40:38 -0300

    Implementação de novos recursos
    -> Implementado o shield Ethernet W5100.
    -> Implementado o sensor de temperatura e umidade DHT22 e sua
    biblioteca.
    -> Implementado o sensor de luminosidade LDR.

--- sketch_InCasa/sketch_InCasa.ino
+++ sketch_InCasa/sketch_InCasa.ino
@@ -1,4 +1,7 @@
 //Projeto InCasa
+//Includes
+#include <Ethernet.h>
+#include <DHT.h>
 
 //Pinos
 //1 - 
@@ -14,6 +17,14 @@
 //11 - 
 //12 - 
 //13 - 
+//A0 - LDR
+
+//Definicoes de IP, mascara de rede e gateway
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress ip(192,168,0,150);          //Define o endereco IP
+IPAddress gateway(192,168,0,1);     //Define o gateway
+IPAddress subnet(255, 255, 255, 0); //Define a mascara
 
 // Pinos Rele
 int rele1 = 7;
@@ -21,13 +32,22 @@ int rele2 = 8;
 int rele3 = 9;
 int rele4 = 10;
 
+// Define pino e sensor DHT22
+DHT dht(5, DHT22);
+
+// Define o LDR
+int ldrPin = 0; //LDR no pino analígico 8
+int ldrValor = 0; //Valor lido do LDR
 
 void setup() {
+  //Inicializa a interface de rede
+  Ethernet.begin(mac, ip, gateway, subnet);
   // Pinos rele para saida
   pinMode(rele1, OUTPUT);
   pinMode(rele2, OUTPUT);
   pinMode(rele3, OUTPUT);
   pinMode(rele4, OUTPUT);
+  dht.begin();
   
 }
 
@@ -70,5 +90,20 @@ void desligaRele(int canal) {
       digitalWrite(rele4, HIGH);
     break;  
   }  
+}
+
+void Temperatura() {
+  float temperatura     = dht.readTemperature();
+}
+
+void Umidade() {
+  float umidade         = dht.readHumidity();
+}
+
+void Luminosidade(){
+  //ler o valor do LDR
+  ldrValor = analogRead(ldrPin); //O valor lido será entre 0 e 1023
+}
+
 }
 
